e2e5b6f1b18b1234a75d55a1a2e14e7de21885d5
Adam Charane <adamcharane@gmail.com>
2019-11-01 04:06:04 +0100

    Adding items to cart

--- core/views.py
+++ core/views.py
@@ -1,17 +1,42 @@
-from django.shortcuts import render
-from .models import Item
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views.generic import DetailView, ListView
+from django.utils import timezone
+from .models import Item, OrderItem, Order
 
 
-def item_list(request):
-    context = {
-        'items': Item.objects.all()
-    }
-    return render(request, "home-page.html", context)
+class HomeView(ListView):
+    model = Item
+    template_name = 'home-page.html'
 
 
 def checkout(request):
     return render(request, 'checkout-page.html')
 
 
-def products(request):
-    return render(request, 'product-page.html')
+class ItemDetailView(DetailView):
+    model = Item
+    template_name = 'product.html'
+
+
+def add_to_cart(request, slug):
+    item = get_object_or_404(Item, slug=slug)
+    order_item, created = OrderItem.objects.get_or_create(
+        item=item,
+        user=request.user,
+        ordered=False
+    )
+    order_qs = Order.objects.filter(user=request.user, ordered=False)
+    if order_qs.exists():
+        order = order_qs[0]
+        # check if the order item is in the order
+        if order.items.filter(item__slug=item.slug).exists():
+            order_item.quantity += 1
+            order_item.save()
+        else:
+            order.items.add(order_item)
+    else:
+        ordered_date = timezone.now()
+        order = Order.objects.create(
+            user=request.user, ordered_date=ordered_date)
+        order.items.add(order_item)
+    return redirect("core:product", slug=slug)
